add4b2ca03b4b6343d97e0c1b417f958a499079b
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-04-08 13:47:37 +0700

    add title web

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="{{asset('public/favicon/stamping.png')}}" type="image/x-icon">
+    <title>Metal Mask</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
   </head>
